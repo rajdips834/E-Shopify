e150e3ea959f8cd711200e6c705c47a57da9345b
Rajdip Sinha <rajdips834@gmail.com>
2023-11-10 11:18:26 +0530

    Dashboard design added

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import {
   Switch,
   Routes,
 } from "react-router-dom";
+import ProductInfo from "./page/productInfo/ProductInfo";
 import MyState from "./context/data/MyState";
 import Home from "./page/home/Home";
 import Order from "./page/order/Order";
 import NoPage from "./page/nopage/NoPage";
 import Cart from "./page/cart/Cart";
-import Dashboard from "./page/dashboard/Dashboard";
+import Dashboard from "./page/admin/dashboard/Dashboard";
+import Login from "./page/auth/SignUp";
+import SignUp from "./page/auth/Login";
 const App = () => {
   return (
     <MyState>
@@ -21,6 +24,9 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="*" element={<NoPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/productinfo/:id" element={<ProductInfo />} />
         </Routes>
       </Router>
     </MyState>
---
+++ src/page/auth/SignUp.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import myContext from "../../context/data/myContext";
+
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const context = useContext(myContext);
+  const { loading, setLoading } = context;
+  const signup = () => {};
+
+  return (
+    <div className="flex items-center justify-center h-screen ">
+      <div className="px-10 py-10 bg-gray-800 rounded-xl">
+        <div className="">
+          <h1 className="mb-4 text-xl font-bold text-center text-white">
+            Signup
+          </h1>
+        </div>
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            name="name"
+            className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
+            placeholder="Name"
+          />
+        </div>
+
+        <div>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
+            placeholder="Email"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
+            placeholder="Password"
+          />
+        </div>
+        <div className="flex justify-center mb-3 ">
+          <button
+            onClick={signup}
+            className="w-full px-2 py-2 font-bold text-white bg-red-500 rounded-lg "
+          >
+            Signup
+          </button>
+        </div>
+        <div>
+          <h2 className="text-white">
+            Have an account{" "}
+            <Link className="font-bold text-red-500 " to={"/login"}>
+              Login
+            </Link>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
